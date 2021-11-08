2fea7bbcfc0445a13503930487445f760ee4647a
IcePingus <pingus32@hotmail.fr>
2021-11-08 11:54:08 +0100

    Modification StepDefinitions

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -12,32 +12,38 @@ public class StepDefinitions {
         o.setOwner(who);
     }
 
-    @When("Jerry is declared as recipient")
-    public void declaring_recipient(){
-      o.setRecipient("Juliet");
+    @Given("{word} who wants to create an Order")
+    public void creating_an_order(String who) {
+        o = new Order();
+        o.setOwner(who);
     }
-    
-    @When("a {string} is added to the order")
+
+    @When("{word} is declared as recipient")
+    public void declaring_recipient(String who){
+        o.setRecipient(who);
+    }
+
+    @When("a(nother?) {string} is added to the order")
     public void add_drink_to_the_order(String drinkName){
         o.getDrinks().add(new Order.Drink(drinkName));
     }
 
-    @Then("the order contains {int} drink")
+    @Then("the order contains {int} drink(s?)")
     public void check_order_size(int size) {
         assertEquals(size, o.getDrinks().size());
+    }
+
+    @Then("the order contains {int} {string}")
+    public void check_order_contents(int size, String drink) {
+        long count = o.getDrinks().stream()
+                .filter(d -> d.getName().equals(drink))
+                .count();
+        assertEquals(size,count);
     }
 
     @Then("the order does not contain any drinks")
     public void check_emptiness() {
         List<Order.Drink> drinks = o.getDrinks();
         assertEquals(0, drinks.size());
-    }
-    
-    @Then("the order contains {int} {string}")
-    public void check_order_contents(int size, String drink) {
-        long count = o.getDrinks().stream()
-                      .filter(d -> d.getName().equals(drink))
-                      .count();
-        assertEquals(size,count);
     }
 }
